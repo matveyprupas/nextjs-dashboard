028bf658c4a08bba80dc1951e5110f4faccd2678
Matvey Prupas <matvey.prupas@ventionteams.com>
2024-01-24 16:27:51 +0200

    feat: finish chapter 9

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,13 @@
-export default function Page() {
+import { fetchRevenue } from "@/app/lib/data";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+
+export default async function Page() {
+  // const revenue = await fetchRevenue();
+
   return (
-    <p>Incoices Page</p>
+    <>
+      <p>Invoices</p>
+      {/* <RevenueChart revenue={revenue}  />       */}
+    </>
   );
 }
